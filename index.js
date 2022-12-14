2ecf048695a45b72d8d0e9d925c742630bd7e0cd
sanidkpc <sanidkpc@gmail.com>
2022-12-15 05:32:54 +0600

    Update text

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+var http = require("http");
+http
+  .createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`);
+    res.write("Yo! sanid");
     res.end();
-}).listen(process.env.PORT || 3000);
+  })
+  .listen(process.env.PORT || 3000);
